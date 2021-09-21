560a4e3167c08353ce6e1ecb81b2228789ef1bc4
Jordan Roberts <jxavier831@gmail.com>
2021-09-21 15:23:13 -0400

    Completed Variables Exercise and Bonus questions

---
+++ script.js
@@ -0,0 +1,26 @@
+// Variables Exercise
+
+// 1
+// Constant or const
+
+// 2
+let num = 20;
+num = 15; // 3
+num += 7; // 4
+num -= 1; // 5
+num = "This is not a number"; // 6
+
+// 7
+let blank;
+console.log(blank); // 8
+blank = null; // 9
+
+// BONUS
+
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+console.log(quarter, dime, nickel, penny); // 2
+// 3
+let letters = "qwertyuiop"
+console.log(letters.length); //4
+console.log(letters[9]); // 4
